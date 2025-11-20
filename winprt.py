9b704f24099cbe4483d01b5c3264628dce70cd2b
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-19 22:55:45 -0500

    create func to open the port 3389

--- winprt.py
+++ winprt.py
@@ -9,7 +9,7 @@ print("[::] 3389     [::] 139      [::] 389     [::] 539      [::] 53")
 print("[::] 445      [::] 135      [::] 636     [::] 3268     [::] 88")
 print("-"*64)
 
-#---------CREATE FIREWALL RULE------------------
+#---------CREATE FIREWALL RULE FUNCTION------------------
 def CreateFirewallRule():
  print("")
  print("Creating firewall rule...")
@@ -22,21 +22,19 @@ def CreateFirewallRule():
         f'-Action Allow '
         f'-Profile Any'
     )
-
-#---------POWERSHELL SCRIPTS AND RESULTS------------------
-
- CheckIfIsAdmin = (  #CHECK IF THE USER IS ADMIN BEFORE OPEN THE PORT
-    '([Security.Principal.WindowsPrincipal] '
-    '[Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole('
-    '[Security.Principal.WindowsBuiltInRole]::Administrator)'
-)
- #FIREWALL RULE, EXECUTE POWERSHELL SCRIPT AND SAVE THE RESULT
+  #FIREWALL RULE, EXECUTE POWERSHELL SCRIPT AND SAVE THE RESULT
  firewallRuleCreationOutput = subprocess.run(
         ["powershell", "-Command", createFirewallRuleComand],
         capture_output=True,  
         text=True
     )
- #CHECK IF IS ADMIN RESULT
+ CheckIfIsAdmin = (  #CHECK IF THE USER IS ADMIN BEFORE OPEN THE PORT
+    '([Security.Principal.WindowsPrincipal] '
+    '[Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole('
+    '[Security.Principal.WindowsBuiltInRole]::Administrator)'
+  )
+ 
+  #CHECK IF IS ADMIN RESULT
  CheckIfIsAdminResult = subprocess.run(
     ["powershell", "-Command", CheckIfIsAdmin],
     capture_output=True,
@@ -44,7 +42,6 @@ def CreateFirewallRule():
 )
  a = firewallRuleCreationOutput.returncode #SAVE THE OUTPUT OF THE PS SCRIPT 
 
- #CHECK IF THE USER IS ADMIN CALLING 
  if CheckIfIsAdminResult.stdout.strip() == "True" and a == 0: 
   print("")
   print(f"Firewall rule was created succesfully!")
@@ -52,13 +49,43 @@ def CreateFirewallRule():
   print("")
   print("Opening the Port...")
   time.sleep(1)
+  if port == "3389":
+   openPort3389()
+
+
+
+
+
+
+
+
+
+
+
  else:
   print("")
-  print("Error creating the firewall rule, in order to use the tool you need to have root priviliges.")
+  print("Error creating the firewall rule, in order to use the tool you need to have root privileges.")
+#---------END CREATE FIREWALL RULE FUNCTION------------------
+
+
+
+ #CHECK IF THE USER IS ADMIN BEFORE OPENING A PORT
+
 
 #---------OPEN PORTS FUNCTIONS--------------------
-def openPort3389():
- openPort3389 = (r'Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0') #ACTIVATE REMOTE DESKTOP IN WINDOWS 
+def openPort3389(): #ACTIVATE REMOTE DESKTOP IN WINDOWS 
+ openPort3389 = (r'Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0') 
+ op3389Result = subprocess.run(
+    ["powershell", "-Command",op3389Result],
+    capture_output=True,
+    text=True
+ )
+ if op3389Result.returncode == 0:
+  time.sleep(1)
+  print(f"Port {port} was openned succesfully! ")
+ else:
+  print(f"Error opening port {port}")
+
 
 def openPort445():
  openPort445 = ('Set-Service -Name "LanmanServer" -StartupType Automatic; Start-Service -Name "LanmanServer"') 

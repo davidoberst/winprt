433ff4602aaddc3414fb1a5551573948b3d801fb
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-19 20:03:31 -0500

    rewrite code

--- winprt.py
+++ winprt.py
@@ -4,15 +4,16 @@ from pyfiglet import figlet_format
 import sys
 print(figlet_format("winprt", font="small"))
 print("v.0.0.1","by davidoberst","https://github.com/davidoberst",sep=" | ")
-print("-"*58)
-print("[1] Open a Port")
-print("[1] Close a Port")
+print("-"*64)
+print("[::] 3389     [::] 139      [::] 389     [::] 539      [::] 53")
+print("[::] 445      [::] 135      [::] 636     [::] 3268     [::] 88")
+print("-"*64)
 
-def openPortTCP():
- time.sleep(1)
- port = (input("PORT --> "))
- 
- comand = (
+#---------CREATE FIREWALL RULE------------------
+def CreateFirewallRule():
+ print("")
+ print("Creating firewall rule...")
+ createFirewallRuleComand = (
         f'New-NetFirewallRule '
         f'-DisplayName "OPENPORT" '
         f'-Direction Inbound '
@@ -22,28 +23,41 @@ def openPortTCP():
         f'-Profile Any'
     )
 
- CheckIfIsAdmin = ( 
+#---------POWERSHELL SCRIPTS AND RESULTS------------------
+
+ CheckIfIsAdmin = (  #CHECK IF THE USER IS ADMIN BEFORE OPEN THE PORT
     '([Security.Principal.WindowsPrincipal] '
     '[Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole('
     '[Security.Principal.WindowsBuiltInRole]::Administrator)'
 )
-
- result = subprocess.run(
-        ["powershell", "-Command", comand],
+ #FIREWALL RULE, EXECUTE POWERSHELL SCRIPT AND SAVE THE RESULT
+ firewallRuleCreationOutput = subprocess.run(
+        ["powershell", "-Command", createFirewallRuleComand],
         capture_output=True,  
         text=True
     )
- CheckIfIsAdminResult = subprocess.run(["powershell", "-Command",CheckIfIsAdmin])
- 
- if CheckIfIsAdminResult == "True":
-  print(f"Output {result.returncode}")
-  print(result.stdout) 
-  print(result.stderr)
- else:
-  print("You need to be Admin to run this tool.")
-  sys.exit()
+ #CHECK IF IS ADMIN RESULT
+ CheckIfIsAdminResult = subprocess.run(
+    ["powershell", "-Command", CheckIfIsAdmin],
+    capture_output=True,
+    text=True
+)
+ a = firewallRuleCreationOutput.returncode #SAVE THE OUTPUT OF THE PS SCRIPT 
 
-option = int(input("--> "))
-if option == 1:
- time.sleep(1)
- openPortTCP()
+ #CHECK IF THE USER IS ADMIN CALLING 
+ if CheckIfIsAdminResult.stdout.strip() == "True" and a == 0: 
+  print("")
+  print(f"Firewall rule was created succesfully!")
+  time.sleep(1)
+  print("")
+  print("Opening the Port...")
+  time.sleep(1)
+ else:
+  print("")
+  print("Error creating the firewall rule, in order to use the tool you need to have root priviliges.")
+  
+#USER ENTRY
+port = input("Wich port do you want to open? -->  ")
+time.sleep(1)
+CreateFirewallRule()
+
